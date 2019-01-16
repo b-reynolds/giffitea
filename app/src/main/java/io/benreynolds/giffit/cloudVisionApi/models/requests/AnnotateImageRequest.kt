0a643a15b5baf5e3262da2e8fe933f558667f830
Ben Reynolds <breynolds1994@outlook.com>
2019-01-16 21:37:01 +0000

    Implement required Retrofit models/services for the Google Vision API annotate method

---
+++ app/src/main/java/io/benreynolds/giffit/cloudVisionApi/models/requests/AnnotateImageRequest.kt
@@ -0,0 +1,9 @@
+package io.benreynolds.giffit.cloudVisionApi.models.requests
+
+/**
+ * Request body to run image detection and annotation for a batch of images.
+ *
+ * @property image Image to be processed.
+ * @property features Requested features.
+ */
+data class AnnotateImageRequest(val image: Image, val features: List<Feature>)
---
+++ app/src/main/java/io/benreynolds/giffit/cloudVisionApi/models/requests/Feature.kt
@@ -0,0 +1,22 @@
+package io.benreynolds.giffit.cloudVisionApi.models.requests
+
+/**
+ * Type of Google Cloud Vision API detection to perform, and the maximum number of results to return
+ * for that type.
+ *
+ * @property Feature Feature type.
+ * @property maxResults Maximum number of results of this type.
+ * @property model Model to use for the feature. Supported values: "builtin/stable" (the default if
+ * unset) and "builtin/latest".
+ */
+data class Feature(val type: String, val maxResults: Int, val model: String) {
+  /**
+   * Type of Google Cloud Vision API feature to be extracted.
+   */
+  companion object Type {
+    /**
+     * Unspecified feature type.
+     */
+    const val LABEL_DETECTION = "LABEL_DETECTION"
+  }
+}
