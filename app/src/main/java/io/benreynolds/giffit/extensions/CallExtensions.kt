5cd992baf1becd933f4bb903e67812ceec4ea09a
Ben Reynolds <breynolds1994@outlook.com>
2019-01-19 16:09:58 +0000

    Create Call.enqueue extension function to allow for more expressive usage

---
+++ app/src/main/java/io/benreynolds/giffit/extensions/CallExtensions.kt
@@ -0,0 +1,12 @@
+package io.benreynolds.giffit.extensions
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+fun <T> Call<T>.enqueueKt(onSuccess: (Response<T>) -> Unit, onFailure: (t: Throwable?) -> Unit) {
+  enqueue(object : Callback<T> {
+    override fun onFailure(call: Call<T>?, throwable: Throwable?) = onFailure(throwable)
+    override fun onResponse(call: Call<T>?, response: Response<T>) = onSuccess(response)
+  })
+}
