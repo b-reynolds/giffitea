6adec9ffeaaa8cb3bcc7d448a0bfa99f3ebfb5fb
Ben Reynolds <breynolds1994@outlook.com>
2019-01-19 16:50:15 +0000

    Implement extensions for converting an image into a Base 64 encoded string

---
+++ app/src/main/java/io/benreynolds/giffit/extensions/FileExtensions.kt
@@ -0,0 +1,13 @@
+package io.benreynolds.giffit.extensions
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.File
+
+fun File.toBitmap(): Bitmap? {
+  return try {
+    BitmapFactory.decodeFile(absolutePath)
+  } catch (exception: IllegalArgumentException) {
+    return null
+  }
+}
